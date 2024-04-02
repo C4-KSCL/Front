836ce5ece5ef1eded90848da59566d2a1bfce8ee
Junyoung <sauljoa3711@gmail.com>
2024-04-02 12:43:13 +0900

    merge

--- lib/pages/signup/imageUpload/selectImagePage.dart
+++ lib/pages/signup/imageUpload/selectImagePage.dart
@@ -14,7 +14,6 @@ class SelectImagePage extends StatefulWidget {
 
 UserDataController userDataController = Get.find<UserDataController>();
 final picker = ImagePicker();
-XFile? image; // 카메라로 촬영한 이미지를 저장할 변수
 List<XFile?> multiImage = []; // 갤러리에서 여러장의 사진을 선택해서 저장할 변수
 List<XFile?> images = []; // 가져온 사진들을 보여주기 위한 변수
 
