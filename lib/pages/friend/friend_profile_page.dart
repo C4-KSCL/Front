509f47ce216aa9f5f990871f06a6219f58ebc138
SanghyunLee <lgw9736@naver.com>
2024-04-20 22:02:41 +0900

    친구 프로필 페이지 - 채팅방 이동 버튼 추가

--- lib/pages/friend/friend_profile_page.dart
+++ lib/pages/friend/friend_profile_page.dart
@@ -1,16 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:frontend_matching/models/friend.dart';
+import 'package:frontend_matching/pages/chatting_room/chatting_room_page.dart';
 import 'package:frontend_matching/theme/colors.dart';
 import 'package:frontend_matching/theme/textStyle.dart';
 import 'package:get/get.dart';
 
-Widget FriendProfilePage({required Friend friendData}){
+Widget FriendProfilePage({required Friend friendData}) {
   return Padding(
     padding: const EdgeInsets.fromLTRB(10, 0, 10, 50),
     child: Container(
       margin: EdgeInsets.all(10.0),
-      width: Get.width*0.6,
-      height: Get.height*0.6,
+      width: Get.width * 0.6,
+      height: Get.height * 0.6,
       decoration: BoxDecoration(
         borderRadius: BorderRadius.circular(10),
         color: Colors.white.withOpacity(0.7),
@@ -23,8 +24,7 @@ Widget FriendProfilePage({required Friend friendData}){
               Row(
                 children: [
                   CircleAvatar(
-                    backgroundImage: NetworkImage(
-                        friendData.userImage),
+                    backgroundImage: NetworkImage(friendData.userImage),
                   ),
                   Column(
                     children: [
@@ -40,9 +40,9 @@ Widget FriendProfilePage({required Friend friendData}){
                             ),
                             child: Center(
                                 child: Text(
-                                  friendData.age,
-                                  style: whiteTextStyle2,
-                                )),
+                              friendData.age,
+                              style: whiteTextStyle2,
+                            )),
                           ),
                         ],
                       ),
@@ -54,20 +54,33 @@ Widget FriendProfilePage({required Friend friendData}){
               IconButton(onPressed: Get.back, icon: Icon(Icons.close))
             ],
           ),
-          SizedBox(height: 10,),
+          SizedBox(
+            height: 10,
+          ),
           Divider(),
-          SizedBox(height: 10,),
+          SizedBox(
+            height: 10,
+          ),
           Container(
             height: 200,
             width: 200,
             color: Colors.black,
           ),
-          SizedBox(height: 10,),
-          Text(friendData.myKeyword)
+          SizedBox(
+            height: 10,
+          ),
+          Text(friendData.myKeyword),
+          // 채팅방 이동 버튼
+          TextButton(
+              onPressed: () {
+                Get.to(ChatRoomPage(
+                  roomId: friendData.roomId!,
+                  oppUserName: friendData.nickname,
+                ));
+              },
+              child: Text("채팅방 이동"))
         ],
       ),
     ),
   );
 }
-
-
