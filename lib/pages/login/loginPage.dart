406bbcac4c91a188c10179a6b36034bdd252e16e
SanghyunLee <lgw9736@naver.com>
2024-06-26 02:21:16 +0900

    💄 로그인 페이지 디자인 수정

--- lib/pages/login/loginPage.dart
+++ lib/pages/login/loginPage.dart
@@ -1,11 +1,12 @@
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:frontend_matching/config.dart';
 import 'package:frontend_matching/controllers/user_data_controller.dart';
 import 'package:frontend_matching/pages/login/bottomLayer.dart';
 import 'package:get/get.dart';
-
+import 'dart:ui' as ui;
 import '../../controllers/bottomNavigationBar.dart';
 import '../chatting_room/chatting_room_page.dart';
 
@@ -28,6 +29,20 @@ class _LoginPageState extends State<LoginPage> {
             child: Image.asset(
               'assets/images/brand.png',
               fit: BoxFit.cover, // 전체 화면으로 배경
+            ),
+          ),
+          Positioned.fill(
+            bottom: Get.height / 2,
+            child: ClipRect(
+              child: BackdropFilter(
+                filter: ui.ImageFilter.blur(sigmaX: 2.0, sigmaY: 2.0),
+                child: Padding(
+                  padding: const EdgeInsets.all(20),
+                  child: SvgPicture.asset(
+                    'assets/images/logo.svg',
+                  ),
+                ),
+              ),
             ),
           ),
           Align(
